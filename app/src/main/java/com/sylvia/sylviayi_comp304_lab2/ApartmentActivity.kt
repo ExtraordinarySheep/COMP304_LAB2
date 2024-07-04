1a80b7230924e19224f9f19959af00626c72d3bf
ExtraordinarySheep <siyuyis@gmail.com>
2024-07-04 08:49:25 -0400

    final

--- app/src/main/java/com/sylvia/sylviayi_comp304_lab2/ApartmentActivity.kt
+++ app/src/main/java/com/sylvia/sylviayi_comp304_lab2/ApartmentActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Button
+
 
 
 class ApartmentActivity : AppCompatActivity() {
